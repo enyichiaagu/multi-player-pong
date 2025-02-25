74602d8e1df117403053c3bd652826bae6a15dbe
enyichiaagu <enyichiaagu@gmail.com>
2025-02-25 10:02:10 +0100

    Adding socket.io

--- server.js
+++ server.js
@@ -2,11 +2,24 @@ import { createServer } from 'node:http';
 import { Server } from 'socket.io';
 
 const httpServer = createServer();
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
 
 const PORT = 3000;
 httpServer.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
+let readyPlayerCount = 0;
+
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log('a user connected', socket.id);
+
+  readyPlayerCount++;
+
+  socket.on('ready', () => {
+    //broadcast('startGame')
+  });
 });
